740ca35d148cfdd9aa54cf8ba7400f2cd5b73c39
Emil Shain <emilshain.official@gmail.com>
2026-08-18 22:00:43 +0530

    dep up

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -16,7 +16,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
 
     'rest_framework',
     'rest_framework_simplejwt',
@@ -75,8 +74,6 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-
-STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
